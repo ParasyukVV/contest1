0ee98d76df85361cd355ebbe8838e08a718823a6
vvp <junior.vladimir@yandex.ru>
2023-10-16 11:06:27 +0300

    Принято на проверку

--- E.cpp
+++ E.cpp
@@ -1,58 +1,83 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <limits>
 
 template <typename Type>
-class FenwickTree {
+class SumSegmentTree {
  private:
-  std::vector<Type> tree_;
+  Type neutral_element_ = 0;
   int size_;
+  std::vector<Type> tree_;  // 0 - индексацию хотим
 
-  int Sum(int right) {
-    Type answer = 0;
-    for (; right >= 0; right = (right & (right + 1)) - 1) {
-      answer += tree_[right];
+  int Parent(int position) {
+    return (position - 1) / 2;
+  }
+
+  int LeftChild(int position) {
+    return 2 * position + 1;
+  }
+
+  int RightChild(int position) {
+    return 2 * position + 2;
+  }
+
+  bool IsLeftChild(int position) {
+    return position % 2 == 1;
+  }
+
+  bool IsRightChild(int position) {
+    return position % 2 == 0;
+  }
+
+  int UpperDegTwo(int x) {
+    int answer = 1;
+    while (answer < x) {
+      answer *= 2;
     }
     return answer;
   }
 
  public:
-  void IncreaseByValue(int position, Type value) {
-    for (; position < size_; position |= position + 1) {
-      tree_[position] += value;
+  explicit SumSegmentTree(const std::vector<Type> &input_array) {
+    int real_size = static_cast<int>(input_array.size());  // Размер копируемого массива
+    size_ = UpperDegTwo(real_size);                        // Теперь считаем размер 2^k
+    tree_.resize(2 * size_ - 1, neutral_element_);
+    for (int i = size_ - 1; i < real_size + size_ - 1; ++i) {
+      tree_[i] = input_array[i - size_ + 1];
+    }
+    for (int i = size_ - 2; i >= 0; --i) {
+      tree_[i] = tree_[LeftChild(i)] + tree_[RightChild(i)];
     }
   }
 
-  void ChangeValue(int position, Type value) {
-    IncreaseByValue(position, value - Sum(position, position));
-  }
-
-  explicit FenwickTree(const std::vector<Type> &array) {
-    size_ = static_cast<int>(array.size());
-    std::vector<Type> prefix_sum;
-    prefix_sum.push_back(0);
-    for (int i = 1; i < size_; ++i) {
-      prefix_sum.push_back(array[i] + prefix_sum[i - 1]);
-    }
-    for (int i = 0; i < size_; ++i) {
-      tree_.push_back(prefix_sum[i] - prefix_sum[(i & (i + 1)) - 1]);
+  void Update(int position, Type value) {
+    position += size_ - 1;
+    tree_[position] = value;
+    while (position != 0) {
+      position = Parent(position);
+      tree_[position] = tree_[LeftChild(position)] + tree_[RightChild(position)];
     }
   }
 
-  explicit FenwickTree(const std::vector<int> &array) {
-    size_ = static_cast<int>(array.size());
-    std::vector<Type> prefix_sum;
-    prefix_sum.push_back(0);
-    for (int i = 1; i < size_; ++i) {
-      prefix_sum.push_back(array[i] + prefix_sum[i - 1]);
+  Type Query(int left, int right) {
+    left += size_ - 1;
+    right += size_ - 1;
+    Type answer = neutral_element_;
+    while (left < right) {
+      if (IsLeftChild(right)) {
+        answer += tree_[right];
+      }
+      if (IsRightChild(left)) {
+        answer += tree_[left];
+      }
+      left = Parent(left + 1);
+      right = Parent(right - 1);
     }
-    for (int i = 0; i < size_; ++i) {
-      tree_.push_back(prefix_sum[i] - prefix_sum[(i & (i + 1)) - 1]);
+    if (left == right) {
+      answer += tree_[left];
     }
-  }
-
-  int Sum(int left, int right) {
-    return Sum(right) - Sum(left - 1);
+    return answer;
   }
 };
 
@@ -61,24 +86,23 @@ int main() {
   std::ofstream cout;
   cin.open("input.txt");
   cout.open("output.txt");
-  int amount = 0;  // N и M
+  int amount = 0;  // N и K
   cin >> amount;
-  std::vector<int> input_array;
-  input_array.push_back(0);  // Остаёмся в 1-индексации
+  std::vector<int64_t> input_array;
   for (int i = 0, value = 0; i < amount; ++i) {
     cin >> value;
     input_array.push_back(value);
   }
-  FenwickTree<int64_t> fnw(input_array);
-  char request_type = 0;
+  SumSegmentTree<int64_t> seg_tree(input_array);
   cin >> amount;
-  for (int i = 0, arg1 = 0, arg2 = 0; i < amount; ++i) {
-    cin >> request_type >> arg1 >> arg2;
+  char request_type = 0;
+  for (int i = 0, left = 0, right = 0; i < amount; ++i) {
+    cin >> request_type >> left >> right;
     if (request_type == 's') {
-      cout << fnw.Sum(arg1, arg2) << ' ';
+      cout << seg_tree.Query(left - 1, right - 1) << ' ';
     }
     if (request_type == 'u') {
-      fnw.ChangeValue(arg1, arg2);
+      seg_tree.Update(left - 1, right);
     }
   }
   cin.close();
